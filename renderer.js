77ea2dd7c6b63fb9fe6b9d7c375f26ad0fb0f7b5
ryanh <ryanhuang6391@gmail.com>
2026-07-08 15:21:14 -0700

    preload script integration

---
+++ renderer.js
@@ -0,0 +1,9 @@
+const information = document.getElementById('info')
+information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+
+const func = async () => {
+    const response = await window.versions.ping()  // window.versions and versions same thing
+    console.log(response)
+}
+
+func()
